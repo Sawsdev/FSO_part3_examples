3cc60f27dc5aa36d19049d06ad1b80140e60c594
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-03-19 06:38:40 -0500

    Add folder scaffolding

--- models/note.js
+++ models/note.js
@@ -1,21 +1,6 @@
 const mongoose = require('mongoose')
 
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-    .then(result => {
-        console.log(result)
-        console.log('connected to MongoDB Atlas')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB Atlas: ', error.message)
-    })
-
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
